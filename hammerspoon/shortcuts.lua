cfbff86a64d4b82dbb02902c1a3a73ef24e289e9
Leonardo Rodrigues Ferreira <leo.rf.developer@gmail.com>
2025-01-18 15:47:49 -0300

    Change shortcut to avoid collision with OS shortcuts

--- hammerspoon/shortcuts.lua
+++ hammerspoon/shortcuts.lua
@@ -1,3 +1,3 @@
-hs.hotkey.bind({'shift', 'cmd'}, 'p', function()
+hs.hotkey.bind({'shift', 'cmd', 'ctrl'}, 'p', function()
     hs.execute("/Applications/flameshot.app/Contents/MacOS/flameshot gui", true)
 end)
