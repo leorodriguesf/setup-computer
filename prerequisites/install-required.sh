e93fb3cec1011d1ae04bc75825ab044676ab7850
Leonardo Rodrigues Ferreira <leo.rf.developer@gmail.com>
2025-01-25 15:17:31 -0300

    Add tools installation following macos just changing OS specific tools

--- prerequisites/install-required.sh
+++ prerequisites/install-required.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env sh
 
 if [ "$(id -u)" -eq 0 ]; then
-    SUDO=""
-
-else
-    SUDO="sudo"
+    alias sudo=""
 fi
 
 # Use sudo to install root privileges to prevent installation failures.
-$SUDO echo "Installing required packages..."
+sudo echo "Installing required packages..."
 
-apt-get update
+sudo apt-get update
 
 export DEBIAN_FRONTEND=noninteractive
 
 if ! command -v git >/dev/null; then
-    apt-get install git -y
+    sudo apt-get install git -y
 fi
 
 if ! command -v python3 >/dev/null; then
-    apt-get install python3 -y
+    sudo apt-get install python3 -y
 fi
 
 if ! command -v ansible >/dev/null; then
-    apt install software-properties-common -y
-    add-apt-repository --yes --update ppa:ansible/ansible
-    apt install ansible -y
+    sudo apt install software-properties-common -y
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible -y
 fi
